cc40b88b54697b281fe6146b764d1c6db2093a36
Isabele Rocha <99506836+isadpr@users.noreply.github.com>
2022-10-11 20:23:53 -0300

    soma impares

---
+++ lista02/SomaImpares.java
@@ -0,0 +1,16 @@
+package lista2_exercicio3;
+
+public class SomaImpares {
+	public static void main (String[] args) {
+		int num;
+		int soma = 0;
+		
+		for(num=1; num<10001; num++) {
+			if (num%2!=0) {
+				soma = soma + num;
+			}
+		}
+		
+		System.out.println (soma);
+	}
+}
